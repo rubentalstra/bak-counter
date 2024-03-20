1bee75cfc8fb6177eeb36947ba12e88ce8e0c78b
Ruben Talstra <RubenTalstra1211@outlook.com>
2024-03-20 21:48:18 +0100

    fixed image + isAdmin as well.

--- middleware/setAdminStatus.js
+++ middleware/setAdminStatus.js
@@ -5,7 +5,7 @@ function setAdminStatus(req, res, next) {
     if (req.user && req.user.email) {
         req.user.isAdmin = adminEmails.includes(req.user.email);
     } else {
-        req.user = { isAdmin: false }; // If no user is logged in, create a dummy user object with isAdmin set to false
+        // req.user = { isAdmin: false }; // If no user is logged in, create a dummy user object with isAdmin set to false
     }
     next();
 }
